bc645d66d9e08def7c7adcd96fdf7ffd9c5b207b
SteamShon <shom83@gmail.com>
2014-12-30 10:31:19 +0900

    add iterative reverse.

--- interview/linkedList/reverse.py
+++ interview/linkedList/reverse.py
@@ -19,6 +19,26 @@ def reverse(head):
     last.next = head
     return (newHead, head)
 
+def reverseIter(head):
+    if head == None or head.next == None: return head
+    # deal with size 2
+    ptr = head.next.next
+    prev = head.next
+    prev2 = head
+    prev3 = None
+    while ptr != None: 
+        prev.next = prev2
+        prev2.next = prev3
+        prev3 = prev2
+        prev2 = prev
+        prev = ptr
+        ptr = ptr.next
+
+    prev.next = prev2
+    prev2.next = prev3
+    return prev
+    
+    
 # prepend element at the front of list
 def prepend(head, node):
     node.next = head
@@ -49,9 +69,11 @@ head = Node(10, None)
 append(head, Node(20, None))
 head = prepend(head, Node(1, None))
 
-printList(head)
-newNode = Node(15, None)
-sortedInsert(head, newNode)
+# printList(head)
+# newNode = Node(15, None)
+# sortedInsert(head, newNode)
+# printList(head)
+head = reverseIter(head)
 printList(head)
 #(head, tmp) = reverse(head)
 
